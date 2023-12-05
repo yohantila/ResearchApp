d38ae8918295f492b3f318c3e5eed44a173e3276
Yohannes <yohantila@gmail.com>
2023-12-05 12:22:53 -0500

    Add Authentication and Authorization

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -4,20 +4,36 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SQLitePCL;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")] //api/users
-    public class UsersController : ControllerBase
+
+    [Authorize]
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)
         {
             _context = context;
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return users;
+        }
+
+        [HttpGet("{id}")]   //api/users/2
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            return user;
         }
 
         /*  [HttpGet]
@@ -34,18 +50,5 @@ namespace API.Controllers
              return user;
          } */
 
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
-        {
-            var users = await _context.Users.ToListAsync();
-            return users;
-        }
-
-        [HttpGet("{id}")]   //api/users/2
-        public async Task<ActionResult<AppUser>> GetUser(int id)
-        {
-            var user = await _context.Users.FindAsync(id);
-            return user;
-        }
     }
 }
